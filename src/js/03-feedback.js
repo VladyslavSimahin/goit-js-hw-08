e9ca3ffac3884b3ee90a1d62fc3c15be7ef3eff5
VladyslavSimahin <vladyslav.simahin@gmail.com>
2023-06-24 19:13:30 -0600

    new

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const key = 'feedback-form-state';
+
+let newArg = JSON.parse(localStorage.getItem(key)) || {};
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    newArg[e.target.name] = e.target.value;
+    localStorage.setItem(key, JSON.stringify(newArg));
+  }, 500)
+);
+
+form.addEventListener('submit', submit);
+
+function submit(e) {
+  e.preventDefault();
+  console.log(newArg);
+  localStorage.removeItem(key);
+  form.reset();
+  newArg = {};
+}
+
+function reload() {
+  for (let el of form) {
+    if (newArg && Object.keys(newArg).includes(el.name)) {
+      el.value = newArg[el.name];
+    }
+  }
+}
+reload();
